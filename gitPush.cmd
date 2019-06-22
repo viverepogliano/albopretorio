e0955687a3d8e669e11b2447c2bafc667109bb83
giamtrot <giamtrot@hotmail.com>
2019-06-22 10:19:29 +0200

    updated

--- gitPush.cmd
+++ gitPush.cmd
@@ -1,12 +1,14 @@
 REM https://viverepogliano:Pogliano20010@github.com/viverepogliano/albopretorio.git
 
-@echo off
-for /f %%f in ('dir /b') do (
+REM @echo off
+set BASE_DIR=%~dp0
+cd %BASE_DIR%
+for /f %%f in ('dir /b /ad') do (
 	
 	echo ">>>>>>>>>>>>>>>>" %%f
 	call git add %%f
 	call git commit -m "Update of %%f"
-	call git push -u origin master
 	
 )
 
+call git push -u origin master
